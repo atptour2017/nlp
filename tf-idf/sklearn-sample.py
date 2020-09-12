444a177f0a229d1cc2447808bf7dce0b247378ec
chendan <atptour2000@sina.com>
2020-09-12 10:17:16 +0800

    new

---
+++ tf-idf/sklearn-sample.py
@@ -0,0 +1,11 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document?',
+]
+vectorizer = TfidfVectorizer()
+tdm = vectorizer.fit_transform(corpus)
+space = vectorizer.vocabulary_
+print(space)
