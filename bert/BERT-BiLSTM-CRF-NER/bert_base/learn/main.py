ab4fa1f86fff2d98784d4c8857540ec881428b13
陈丹 <49615154@qq.com>
2020-09-12 16:17:58 +0800

    new

---
+++ bert/BERT-BiLSTM-CRF-NER/bert_base/learn/main.py
@@ -0,0 +1,143 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import os
+
+from bert.bert import tokenization
+import tensorflow as tf
+
+
+
+sys.path.append('../')
+sys.path.append('../..')
+from bert_base.server.helper import set_logger
+logger = set_logger('NER Training')
+
+from bert_base.train import tf_metrics
+from bert_base.bert import modeling
+from bert_base.bert import optimization
+from bert_base.bert import tokenization
+
+from train.train_helper import get_args_parser
+from bert_base.train.bert_lstm_ner import *
+
+def learn_train(args):
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device_map
+
+    processors = {
+        "ner": NerProcessor
+    }
+    bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
+
+    if args.max_seq_length > bert_config.max_position_embeddings:
+        raise ValueError(
+            "Cannot use sequence length %d because the BERT model "
+            "was only trained up to sequence length %d" %
+            (args.max_seq_length, bert_config.max_position_embeddings))
+
+    # 在re train 的时候，才删除上一轮产出的文件，在predicted 的时候不做clean
+    if args.clean and args.do_train:
+        if os.path.exists(args.output_dir):
+            def del_file(path):
+                ls = os.listdir(path)
+                for i in ls:
+                    c_path = os.path.join(path, i)
+                    if os.path.isdir(c_path):
+                        del_file(c_path)
+                    else:
+                        os.remove(c_path)
+
+            try:
+                del_file(args.output_dir)
+            except Exception as e:
+                print(e)
+                print('pleace remove the files of output dir and NERdata.conf')
+                exit(-1)
+
+    #check output dir exists
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    processor = processors[args.ner](args.output_dir)
+
+    tokenizer = tokenization.FullTokenizer(
+        vocab_file=args.vocab_file, do_lower_case=args.do_lower_case)
+    session_config = tf.ConfigProto(
+        log_device_placement=False,
+        inter_op_parallelism_threads=0,
+        intra_op_parallelism_threads=0,
+        allow_soft_placement=True)
+
+    run_config = tf.estimator.RunConfig(
+        model_dir=args.output_dir,
+        save_summary_steps=500,
+        save_checkpoints_steps=500,
+        session_config=session_config
+    )
+
+    train_examples = None
+    eval_examples = None
+    num_train_steps = None
+    num_warmup_steps = None
+
+    if args.do_train and args.do_eval:
+        # 加载训练数据
+        train_examples = processor.get_train_examples(args.data_dir)
+        num_train_steps = int(
+            len(train_examples) * 1.0 / args.batch_size * args.num_train_epochs)
+        if num_train_steps < 1:
+            raise AttributeError('training NERdata is so small...')
+        num_warmup_steps = int(num_train_steps * args.warmup_proportion)
+
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", len(train_examples))
+        logger.info("  Batch size = %d", args.batch_size)
+        logger.info("  Num steps = %d", num_train_steps)
+
+        eval_examples = processor.get_dev_examples(args.data_dir)
+
+        # 打印验证集数据信息
+        logger.info("***** Running evaluation *****")
+        logger.info("  Num examples = %d", len(eval_examples))
+        logger.info("  Batch size = %d", args.batch_size)
+
+    label_list = processor.get_labels()
+    # 返回的model_dn 是一个函数，其定义了模型，训练，评测方法，并且使用钩子参数，加载了BERT模型的参数进行了自己模型的参数初始化过程
+    # tf 新的架构方法，通过定义model_fn 函数，定义模型，然后通过EstimatorAPI进行模型的其他工作，Es就可以控制模型的训练，预测，评估工作等。
+    model_fn = model_fn_builder(
+        bert_config=bert_config,
+        num_labels=len(label_list) + 1,
+        init_checkpoint=args.init_checkpoint,
+        learning_rate=args.learning_rate,
+        num_train_steps=num_train_steps,
+        num_warmup_steps=num_warmup_steps,
+        args=args)
+
+    params = {
+        'batch_size': args.batch_size
+    }
+
+    estimator = tf.estimator.Estimator(
+        model_fn,
+        params=params,
+        config=run_config)
+
+    if args.do_train and args.do_eval:
+        # 1. 将数据转化为tf_record 数据
+        train_file = os.path.join(args.output_dir, "train.tf_record")
+        if not os.path.exists(train_file):
+            filed_based_convert_examples_to_features(
+                train_examples, label_list, args.max_seq_length, tokenizer, train_file, args.output_dir)
+
+if __name__=='__main__':
+    args = get_args_parser()
+    if True:
+        import sys
+        param_str = '\n'.join(['%20s = %s' % (k, v) for k, v in sorted(vars(args).items())])
+        print('usage: %s\n%20s   %s\n%s\n%s\n' % (' '.join(sys.argv), 'ARG', 'VALUE', '_' * 50, param_str))
+    print(args)
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device_map
+    learn_train(args=args)
+
